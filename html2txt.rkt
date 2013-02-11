9c28c7a3fc1764c6b34eb13893586816f3082224
dk <dk@porter>
2013-02-12 03:28:59 +0400

    looks like it works

--- html2txt.rkt
+++ html2txt.rkt
@@ -1,29 +1,52 @@
 #lang racket
 
+(provide h2t)
+
 (require (planet neil/html-parsing:2:0))
 
-(define parsed
+(define (file->xexp filename)
   (html->xexp 
-   (open-input-file #:mode 'text "/tmp/index.html")))
+   (open-input-file #:mode 'text filename)))
 
 (define ignored-tags '(*COMMENT* *DECL* @ script style))
-(define br-tags '(table title br div p form select tr))
+(define br-tags '(table title br div p form select tr pre))
 (define space-tags '(span option))
+(define list-tags '(ol ul))
+
+(define links '())
 
 (define (tag-parser contents)
   (let ([tag (car contents)]
         [text (cdr contents)])
-    (cond [(member tag ignored-tags) '("")]
+    (cond [(member tag ignored-tags) '()]
           [(member tag space-tags) (list (parse-xexp text) " ")]
           [(member tag br-tags) (list (parse-xexp text) "\n")]
+          [(member tag list-tags) (list "\n" (parse-xexp text) "\n")]
           [(eq? tag 'td) (list (parse-xexp text) "\t")]
+          [(eq? tag 'li) (list "* " (parse-xexp text) "\n")]
           [(eq? tag 'a)
-           (list "[" (parse-xexp text) "]")]
+             (list "[" (parse-xexp text) "]")]
           [else #f])))
 
+(define (href-parser contents)
+    (let ([tag (car contents)]
+          [text (cdr contents)])
+      (if (eq? tag 'href)
+          (list (car text) "\n")
+          #f)))
+      
+
+(define (parse-links contents) 
+  (cond [(empty? contents) ""]
+        [(list? contents)
+         (apply string-append
+                (or (href-parser contents)
+                    (list (parse-links (car contents)) 
+                          (parse-links (cdr contents)))))]
+        [else ""]))
 
 (define (despace str)
-  (regexp-replace* #rx"^[ \t\n]+|[ \t\n]+$" str ""))
+  (regexp-replace* #rx"^[ \t\n]{2,}|[ \t\n]+$" str ""))
 
 (define (parse-xexp contents)
   (cond [(empty? contents) ""]
@@ -36,7 +59,17 @@
          (despace contents)]
         [else ""]))
 
-(with-output-to-file "/tmp/output" 
-  (lambda () (display (parse-xexp parsed))) #:exists 'replace)
+(define (h2t filename)
+  (with-output-to-file (string-append filename ".txt")  #:exists 'replace
+    (lambda ()
+      (display
+       (let ([xexp (file->xexp filename)])
+         (list
+          (parse-xexp xexp)
+          (parse-links xexp)))))))
+
+
+
+(h2t "/tmp/index.html")
 
 
